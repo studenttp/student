361621494a73328de8d1a1136b270a02a9aacfc6
studenttp <studenttpoints@gmail.com>
2026-07-21 15:36:52 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const clappingSound = new Audio("assets/sounds/clapping.mp3");
+const clickSound = new Audio("assets/sounds/click.wav");
+const magicSound = new Audio("assets/sounds/magic.mp3");
+const shineSound = new Audio("assets/sounds/shine.mp3");
+const takSound = new Audio("assets/sounds/tak.mp3");
+
 // ===================================
 // نجوم صفي
 // سيتم استبدال هذه البيانات بـ Firebase لاحقًا
