09db0dd461ceba2bf8c762370b6f08756efee9bd
studenttp <studenttpoints@gmail.com>
2026-07-21 16:09:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,103 @@ function playError(){
 
 }
 
+const code=document
+.getElementById("studentCode")
+.value.trim();
 
+const result=document
+.getElementById("result");
+
+
+if(code===""){
+
+result.innerHTML=`
+
+<div class="error">
+
+يرجى إدخال الرمز.
+
+</div>
+
+`;
+
+return;
+
+}
+
+
+
+const student=students.find(
+
+s=>s.code===code
+
+);
+
+
+
+if(!student){
+
+result.innerHTML=`
+
+<div class="error">
+
+❌ الرمز غير صحيح
+
+</div>
+
+`;
+
+return;
+
+}
+
+
+
+result.innerHTML=`
+
+<div class="student-card">
+
+<h2>
+
+🌟 ${student.name}
+
+</h2>
+
+<div class="points">
+
+⭐ عدد النجوم
+
+<h1>
+
+${student.star}
+
+</h1>
+
+</div>
+
+<div class="points">
+
+🏆 مجموع النقاط
+
+<h1>
+
+${student.points}
+
+</h1>
+
+</div>
+
+<p>
+
+${student.message}
+
+</p>
+
+</div>
+
+`;
+
+}
 
 // تغيير اسم نجم الشهر
 
