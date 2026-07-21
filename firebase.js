39cef6be258c0881993a4117b955fb6723b70049
studenttp <studenttpoints@gmail.com>
2026-07-21 19:02:29 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,37 +3,24 @@
 // نجوم صفي
 // =====================================
 
-
-// ضعي بيانات مشروع Firebase هنا بعد إنشاء المشروع
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDTtYM0Ifg-6jKZWbcgkCOkWvDIxpukmzM",
   authDomain: "class-rewards-1556c.firebaseapp.com",
   projectId: "class-rewards-1556c",
   storageBucket: "class-rewards-1556c.firebasestorage.app",
   messagingSenderId: "927135908979",
-  appId: "1:927135908979:web:6cda0994a9fd3fa77e9606",
-  measurementId: "G-CDBXQ81BDY"
+  appId: "1:927135908979:web:6cda0994a9fd3fa77e9606"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
+// تشغيل Firebase
 
-import { getFirestore } from "firebase/firestore";
+firebase.initializeApp(firebaseConfig);
 
 
 // قاعدة البيانات
 
-export const db = getFirestore(app);
+const db = firebase.firestore();
 
 // =====================================
 // إضافة طالب
